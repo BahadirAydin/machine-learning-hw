f249612759122f6e5d47516f916f112f99a032a7
BahadirAydin <bahadiraydin1923@gmail.com>
2023-12-07 18:51:42 +0300

    pca added

--- hw2/src/Part2/part2_dimensionalityreduction.py
+++ hw2/src/Part2/part2_dimensionalityreduction.py
@@ -6,6 +6,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.manifold import TSNE
 from umap import UMAP
+from sklearn.decomposition import PCA
 import seaborn as sns
 
 dataset1 = pickle.load(open("../data/part2_dataset_1.data", "rb"))
@@ -69,22 +70,35 @@ def umap_visualization(
     plt.close()
 
 
-# Apply t-SNE with 2 components and perplexity of 30
+def pca_visualization(data, n_components, title):
+    pca = PCA(n_components=n_components)
+    reduced_data = pca.fit_transform(data)
+    cmap = plt.colormaps["viridis"]
+    plt.scatter(
+        reduced_data[:, 0],
+        reduced_data[:, 1],
+        c=cmap(np.arange(len(data)) / len(data)),
+        cmap=cmap,
+    )
+
+    plt.xlabel("PCA1")
+    plt.ylabel("PCA2")
+    plt.title("PCA Visualization ({} components)".format(n_components))
+    plt.colorbar()
+    plt.savefig(title)
+    plt.close()
+
+
 tsne_visualization(dataset1, 2, 30, "tsne-2comps-dataset1.png")
-print("Saved tsne-2comps-dataset1.png")
 tsne_visualization(dataset2, 2, 30, "tsne-2comps-dataset2.png")
-print("Saved tsne-2comps-dataset2.png")
 
 tsne_visualization(dataset1, 3, 30, "tsne-3comps-dataset1.png")
-print("Saved tsne-3comps-dataset1.png")
 tsne_visualization(dataset2, 3, 30, "tsne-3comps-dataset2.png")
-print("Saved tsne-3comps-dataset2.png")
 
 
-tsne_visualization(dataset1, 2, 100, "tsne-100perp-dataset1.png")
-print("Saved tsne-100perp-dataset1.png")
-tsne_visualization(dataset2, 2, 100, "tsne-100perp-dataset2.png")
-print("Saved tsne-100perp-dataset2.png")
+pca_visualization(dataset1, 2, "pca-2comps-dataset1.png")
+pca_visualization(dataset2, 2, "pca-2comps-dataset2.png")
+
 
 # Apply UMAP with 2 components
 umap_visualization(
@@ -93,14 +107,12 @@ umap_visualization(
     title="UMAP 2 components Dataset 1",
     filename="umap-2comps-dataset1.png",
 )
-print("Saved umap-2comps-dataset1.png")
 umap_visualization(
     data=dataset2,
     n_components=2,
     title="UMAP 2 components Dataset 2",
     filename="umap-2comps-dataset2.png",
 )
-print("Saved umap-2comps-dataset2.png")
 
 umap_visualization(
     data=dataset1,
@@ -108,30 +120,12 @@ umap_visualization(
     title="UMAP 2 components Dataset 1",
     filename="umap-3comps-dataset1.png",
 )
-print("Saved umap-3comps-dataset1.png")
 umap_visualization(
     data=dataset2,
     n_components=3,
     title="UMAP 2 components Dataset 2",
     filename="umap-3comps-dataset2.png",
 )
-print("Saved umap-3comps-dataset2.png")
 
-umap_visualization(
-    data=dataset1,
-    n_neighbors=100,
-    title="UMAP 2 components Dataset 1",
-    filename="umap-100neighbors-dataset1.png",
-)
-print("Saved umap-100neighbors-dataset1.png")
-
-umap_visualization(
-    data=dataset2,
-    n_neighbors=100,
-    title="UMAP 2 components Dataset 2",
-    filename="umap-100neighbors-dataset2.png",
-)
-print("Saved umap-100neighbors-dataset2.png")
 
 print("Done!")
-
