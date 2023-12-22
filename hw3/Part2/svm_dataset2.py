3d1263105b9b9fbe9bfb75c971d224325b5fa197
BahadirAydin <bahadiraydin1923@gmail.com>
2023-12-22 15:09:44 +0300

    initial svm_dataset2 implementation

--- hw3/Part2/svm_dataset2.py
+++ hw3/Part2/svm_dataset2.py
@@ -1,5 +1,23 @@
 import pickle
 import numpy as np
+from sklearn.svm import SVC
+from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+
+configurations = [
+    {"C": 0.5, "kernel": "rbf"},
+    {"C": 1, "kernel": "rbf"},
+    {"C": 2, "kernel": "rbf"},
+    {"C": 0.5, "kernel": "linear"},
+    {"C": 1, "kernel": "linear"},
+    {"C": 2, "kernel": "linear"},
+]
 
 dataset, labels = pickle.load(open("../data/part2_dataset2.data", "rb"))
 
+svm = SVC()
+clf = GridSearchCV(svm, configurations, scoring="accuracy")
+X_train, X_test, y_train, y_test = train_test_split(dataset, labels, test_size=0.2)
+
+scaler = StandardScaler()
