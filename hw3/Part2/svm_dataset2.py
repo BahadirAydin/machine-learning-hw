1fa179db0525974ada38ca6cb557ac2da3c90424
BahadirAydin <bahadiraydin1923@gmail.com>
2023-12-24 14:50:47 +0300

    part 2

--- hw3/Part2/svm_dataset2.py
+++ hw3/Part2/svm_dataset2.py
@@ -1,23 +1,45 @@
 import pickle
 import numpy as np
+import pandas as pd
 from sklearn.svm import SVC
 from sklearn.model_selection import GridSearchCV
-from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import Pipeline
+from sklearn.model_selection import StratifiedKFold
 
-configurations = [
-    {"C": 0.5, "kernel": "rbf"},
-    {"C": 1, "kernel": "rbf"},
-    {"C": 2, "kernel": "rbf"},
-    {"C": 0.5, "kernel": "linear"},
-    {"C": 1, "kernel": "linear"},
-    {"C": 2, "kernel": "linear"},
-]
+configurations = {
+    "svm__kernel": (
+        "linear",
+        "rbf",
+    ),
+    "svm__C": [0.1, 1, 10],
+}
 
 dataset, labels = pickle.load(open("../data/part2_dataset2.data", "rb"))
 
-svm = SVC()
-clf = GridSearchCV(svm, configurations, scoring="accuracy")
-X_train, X_test, y_train, y_test = train_test_split(dataset, labels, test_size=0.2)
-
 scaler = StandardScaler()
+svm = SVC()
+pipeline = Pipeline([("scaler", scaler), ("svm", svm)])
+f = StratifiedKFold(n_splits=10, shuffle=True)
+clf = GridSearchCV(
+    pipeline,
+    param_grid=configurations,
+    scoring="accuracy",
+    cv=f,
+    refit=True,
+)
+
+all_scores = [0] * len(configurations["svm__kernel"]) * len(configurations["svm__C"])
+params = []
+N = 5
+for _ in range(N):
+    clf.fit(dataset, labels)
+    df = pd.DataFrame(clf.cv_results_)
+    params = df["params"]
+    scores = df["mean_test_score"]
+    for j in range(len(params)):
+        all_scores[j] += scores[j]
+
+mean_scores = [x / N for x in all_scores]
+for i in range(len(params)):
+    print(params[i], mean_scores[i])
